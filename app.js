4d671a96aca01408903560dd8ebbdbf030e1d088
Munem Prionto <munem.prionto@gmail.com>
2020-06-26 15:20:37 +0600

    added emoji

--- app.js
+++ app.js
@@ -11,44 +11,48 @@ form.addEventListener('submit' , e=> {
     e.preventDefault();
 
     if(+min.value < +max.value) {
-
-        let ranNumb = Math.floor(Math.random() * (+max.value  - + min.value + 1)) + +min.value;
-        output.innerHTML = `<span>${ranNumb}</span>`;
-        
-
-        // counting animation
-/*         numberArray.unshift(ranNumb);
-        let previousNumber = numberArray[1];
-        let currentNumber = numberArray[0];
-        
-        
-        const counting = setInterval(() => {
+        if((+max.value - +min.value) > 100000000) {
+            output.innerHTML = `<p style="font-size:100px">&#128565;</p>`;
+        } else {
+            let ranNumb = Math.floor(Math.random() * (+max.value  - + min.value + 1)) + +min.value;
+            output.innerHTML = `<span>${ranNumb}</span>`;
             
-         
-            if(previousNumber > currentNumber) {
-                previousNumber--;
-                 c = true;
-                if (previousNumber === currentNumber){
-                    clearInterval(counting);
-                    c =false;
-
-                }
-                output.innerHTML = `<span>${previousNumber}</span>`;
+    
+            // counting animation
+    /*         numberArray.unshift(ranNumb);
+            let previousNumber = numberArray[1];
+            let currentNumber = numberArray[0];
+            
+            
+            const counting = setInterval(() => {
                 
-            } else if(previousNumber < currentNumber){
-                previousNumber++;
-                c =true;
-                if (previousNumber === currentNumber){
-                    clearInterval(counting);
-                    c=false;
+             
+                if(previousNumber > currentNumber) {
+                    previousNumber--;
+                     c = true;
+                    if (previousNumber === currentNumber){
+                        clearInterval(counting);
+                        c =false;
+    
+                    }
+                    output.innerHTML = `<span>${previousNumber}</span>`;
+                    
+                } else if(previousNumber < currentNumber){
+                    previousNumber++;
+                    c =true;
+                    if (previousNumber === currentNumber){
+                        clearInterval(counting);
+                        c=false;
+                    }
+                    output.innerHTML = `<span>${previousNumber}</span>`;
+                   
                 }
-                output.innerHTML = `<span>${previousNumber}</span>`;
                
-            }
-           
-        }, 1);
-        
-     */
+            }, 1);
+            
+         */
+        }
+
 
     } else {
         alert('Min value is greater then Max value');
