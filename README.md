6cdffe1f85f9fa2450a528e89bdc5a1a0be38fb1
Munem Prionto <munem.prionto@gmail.com>
2020-06-26 14:56:50 +0600

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+# Number Generator
+
+## Generates a random number by taking minimum and maximum value
--- app.js
+++ app.js
@@ -2,23 +2,65 @@ const output = document.querySelector('#output');
 const min = document.querySelector('#min');
 const max = document.querySelector('#max');
 const form = document.querySelector('#form');
+var numberArray = [0];
+
 
 /* form submit */
 form.addEventListener('submit' , e=> {
+
     e.preventDefault();
+
     if(+min.value < +max.value) {
+
         let ranNumb = Math.floor(Math.random() * (+max.value  - + min.value + 1)) + +min.value;
-        //let ranNumb = Math.floor(Math.random() * (parseInt(max.value)  - parseInt(min.value) + 1)) + parseInt(min.value);
         output.innerHTML = `<span>${ranNumb}</span>`;
+        
+
+        // counting animation
+/*         numberArray.unshift(ranNumb);
+        let previousNumber = numberArray[1];
+        let currentNumber = numberArray[0];
+        
+        
+        const counting = setInterval(() => {
+            
+         
+            if(previousNumber > currentNumber) {
+                previousNumber--;
+                 c = true;
+                if (previousNumber === currentNumber){
+                    clearInterval(counting);
+                    c =false;
+
+                }
+                output.innerHTML = `<span>${previousNumber}</span>`;
+                
+            } else if(previousNumber < currentNumber){
+                previousNumber++;
+                c =true;
+                if (previousNumber === currentNumber){
+                    clearInterval(counting);
+                    c=false;
+                }
+                output.innerHTML = `<span>${previousNumber}</span>`;
+               
+            }
+           
+        }, 1);
+        
+     */
+
     } else {
         alert('Min value is greater then Max value');
     }
-    form.reset();
-    
+
+     form.reset();
 
 });
 
-/* borde color */
+
+
+/* border color */
 min.addEventListener('keyup', e=> {
     if(+min.value > +max.value) {
        min.classList.add('red')
@@ -35,5 +77,3 @@ max.addEventListener('keyup', e=> {
 
 
 
-
-   // oninvalid="setCustomValidity('R U STUPID DUMB ')" oninput="setCustomValidity('')"
